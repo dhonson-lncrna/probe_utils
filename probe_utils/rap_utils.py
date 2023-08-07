c710c921cce9c6a989bf04b0832df6a8bbabce51
Drew Honson <dhonson@lncrna.caltech.edu>
2023-08-07 10:22:33 -0700

    Added BLAT and DFAM utilities for probe generator

--- probe_utils/rap_utils.py
+++ probe_utils/rap_utils.py
@@ -1,61 +1,384 @@
-from Bio import SeqIO
 from Bio.Seq import Seq
-from Bio.SeqFeature import FeatureLocation
-from Bio.SeqFeature import CompoundLocation
+from gget import blat
 
 import pandas as pd
 import numpy as np
 
-def exon_intron(genbank, gene, _type_="ncRNA"):
-    """Takes a genbank file and returns intron and exon sequences"""
-    # Read in and extract the sequence
-    record = SeqIO.read(genbank, "genbank")
+from tqdm import tqdm
 
-    sequence = record.seq
+import sys
+import time
+import threading
+import requests
 
-    # Extract the desired feature
-    feature = []
+class Spinner:
+    busy = False
+    delay = 0.1
+
+    @staticmethod
+    def spinning_cursor():
+        while 1: 
+            for cursor in '|/-\\': yield cursor
+
+    def __init__(self, delay=None):
+        self.spinner_generator = self.spinning_cursor()
+        if delay and float(delay): self.delay = delay
+
+    def spinner_task(self):
+        while self.busy:
+            sys.stdout.write(next(self.spinner_generator))
+            sys.stdout.flush()
+            time.sleep(self.delay)
+            sys.stdout.write('\b')
+            sys.stdout.flush()
+
+    def __enter__(self):
+        self.busy = True
+        threading.Thread(target=self.spinner_task).start()
+
+    def __exit__(self, exception, value, tb):
+        self.busy = False
+        time.sleep(self.delay)
+        if exception is not None:
+            return False
+        
+def blatfilt(seq,
+             genome,
+             probels,
+             tolerance=25):
+    '''
+    Runs UCSC Blat to remove probes with off-target homology
     
-    for i in record.features:
-        if i.type == _type_ and i.qualifiers["gene"] == gene:
-            feature.append(i)
-
-    if len(feature) < 1:
-        raise ValueError("Feature not found")
-
-    elif len(feature) > 1:
-        raise ValueError("More than one feature of given description")
-
-    else:
-        location = feature[0].location
-
-    # Use the exon indices to extract the mature transcript
-    mat_rna = location.extract(sequence)
+    Parameters
+    __________
     
-    # Make a list of exon indices
-    exon_ls = list(location)
+    seq : str
+        Full length of transcript sequence
+        
+    genome : str
+        Short assembly name for the species genome as listed in BLAT, 
+        e.g. 'hg38,' 'mm39,' or 'dm6'
+        
+    probels : list of strings
+        List of probes to be analyzed
+        
+    tolerance : int
+        Number of acceptable matches to other genomic loc. Default 25
+        
+    Returns
+    _______
     
-    # Make a list of intron indices using np.s_
-    intron_slices = []
+    output : two dataframes
+        BLAT results for probes that passed thresholds and those that
+        did not
+        
+    '''
     
-    for i, v in enumerate(exon_ls):
-
-        if v != exon_ls[i - 1] + 1 and v > exon_ls[i - 1]:
-            intron_slices.append(np.s_[exon_ls[i - 1] : v])
-
+    # BLAT full sequence to identify gene location
+    if len(seq) > 8000:
+        seqloc = blat(seq[:8000], assembly=genome, verbose=False).loc[0]
+        endhandle = blat(seq[-1000:], assembly=genome, verbose=False).loc[0]
+        if seqloc['strand'] == '+':
+            seqloc['end'] = endhandle['end']
         else:
+            seqloc['start'] = endhandle['start']
+    else:
+        seqloc = blat(seq, assembly=genome, verbose=False).loc[0]
+    
+    # Check whether entire sequence is present in the indicated genome
+    if seqloc['%_matched'] != 100:
+        percmatch = str(seqlococ['%_matched']) + '%'
+        matcherr = input('The submitted sequence had only a ' + percmatch + 
+                         ' match to the target genome.\n Proceed anyway? [Y/N]')
+        if matcherr == 'Y':
             pass
+        else:
+            raise Exception('Job cancelled.')
+    else:
+        pass
+    
+    # Reformat probe list for BLAT submission
+    blat_ls = ['>'+str(i)+'\n'+v for i,v in enumerate(probels)]
+    
+    # Generate indices to comply with BLAT submission limits
+    inds = list(np.arange(0,len(blat_ls),25))
 
-    # Extract the intron sequences
-    introns = [sequence[i] for i in intron_slices]
+    if inds[-1] != len(blat_ls):
+        inds.append(len(blat_ls))
 
-    return mat_rna, introns
+    sinds = []
+
+    for i,v in enumerate(inds[:-1]):
+        sinds.append(np.s_[v:inds[i+1]])
+
+    # BLAT server URL
+    blat_url = "https://genome.ucsc.edu/cgi-bin/hgBlat"
+    
+    # Make list to store dataframes for each BLAT query
+    df_ls = []
+    
+    # Submit BLAT requests in groups of 25 sequences
+    for i in tqdm(sinds):
+        sequence_data='\n'.join(blat_ls[i])
+        
+        # Parameters for the BLAT request
+        params = {
+            'userSeq': sequence_data,
+            'type': 'dna',
+            'output': 'json',
+            'db':genome
+        }
+
+        # Perform BLAT search
+        response = requests.get(blat_url, params=params)
+        json = response.json()
+
+        df_ls.append(pd.DataFrame(json['blat'],columns=json['fields']))
+    
+    # Join dataframes for entire BLAT set
+    dfall = pd.concat(df_ls)
+    
+    # Drop unused columns
+    dropcols = ['qNumInsert',
+            'qBaseInsert',
+            'tNumInsert',
+            'tBaseInsert',
+            'strand',
+            'misMatches',
+            'repMatches',
+            'nCount',
+            'qSize',
+            'qStart',
+            'qEnd',
+            'tSize',
+            'blockCount',
+            'blockSizes']
+    
+    dfall.drop(dropcols,axis=1,inplace=True)
+    
+    # Generate empty lists to store probe IDs
+    keep_probes = []
+    drop_probes = []
+
+    # Generate query names
+    qnames = [str(i) for i in range(len(probels))]
+
+    # Check if all probes were run
+    if all([i in np.unique(dfall['qName']) for i in qnames]):
+        pass
+    else:
+        raise Exception('''Not all probes were successfully blatted. This is likely due to 
+                        submitting too many requests to the BLAT web server. Try splitting 
+                        your sequence(s) into smaller submissions.''')
+
+    # Loop through individual probes
+    chrom = seqloc['chromosome']
+    for q in qnames:
+        subdf = dfall[dfall['qName'] == q].copy()
+
+        # Remove in-gene matches
+        chrdf = subdf[subdf['tName'] == chrom]
+
+        for i in chrdf.index:
+            if chrdf.loc[i,'tStart'] >= seqloc['start'] and chrdf.loc[i,'tEnd'] <= seqloc['end']:
+                subdf.drop(i,inplace=True)
+            else:
+                pass
+
+        # Check against user-defined acceptable matches
+        if all([i < tolerance for i in subdf['matches']]):
+            keep_probes.append(q)
+        else:
+            drop_probes.append(q)
+
+    keepdf = dfall[dfall['qName'].isin(keep_probes)]
+    dropdf = dfall[dfall['qName'].isin(drop_probes)]
+
+    return keepdf, dropdf
+
+def dfamfilt(probels,
+             species):
+    '''
+    Runs RAP probes through DFAM database to remove 
+    probes to repetitive elements
+    
+    Parameters
+    __________
+    
+    probels : list of strings
+        List of probes to be analyzed
+        
+    species : str
+        DFAM species to check repeats, e.g. "Homo sapiens",
+        "Mus musculus", or "Drosophila melanogaster"
+        
+    Returns
+    _______
+    
+    output : two lists
+        Lists of DFAM results for probes with repeats and those without.
+        
+    file : csv
+        A csv containing DFAM results for probes with repeats.
+        
+    '''
+    
+    url = "https://dfam.org/api/searches"
+    
+    ls = ['>'+str(i)+'\n'+v for i,v in enumerate(probels)]
+    sequence_data = '\n'.join(ls)
+
+    params = {'sequence':sequence_data,
+              'organism':species,
+              'cutoff':'curated'}
+
+    response = requests.post(url, json=params)
+    results = response.json()['id']
+    
+    api_url = f"https://dfam.org/api/searches/{results}"
+
+    response = requests.get(api_url)
+
+    if response.status_code == 200:
+        print("Search submitted successfully.")
+        # Process and use the result data as needed
+    else:
+        raise Exception("Failed to retrieve DFAM result. Status code: " + response.status_code)
+        
+    # Dfam for multiple sequences takes some time
+    # Attempt to get response until job finished
+    response = requests.get(api_url)
+    x = response.json()['duration']
+
+    print('Dfam Starting')
+    
+    with Spinner():
+        while x == 'Not finished':
+            time.sleep(2)
+            response = requests.get(api_url)
+            x = response.json()['duration']
+
+    print('DFAM Done')
+    
+    results = response.json()['results']
+    
+    keep_probes = []
+    drop_probes = []
+    
+    drop_dict = {'probe':[],
+                 'query':[],
+                 'type':[],
+                 'e_value':[]}
+    
+    for r in results:
+        if len(r['hits']) == 0:
+            keep_probes.append(r['query'])
+        else:
+            drop_probes.append(r['query'])
+            
+            # Add details for export
+            for i in r['hits']:
+                drop_dict['probe'].append(probels[int(r['query'])])
+                drop_dict['query'].append(i['query'])
+                drop_dict['type'].append(i['type'])
+                drop_dict['e_value'].append(i['e_value'])
+                
+    # Export DFAM results
+    df = pd.DataFrame(drop_dict)
+    df.to_csv('dfamFailedProbes.csv',index=False)
+    
+    return keep_probes, drop_probes
 
 def rap_probes(fasta, 
                gene, 
                adaptseq = 'CAAGTCA',
-               probe_length = 90):
-    '''Takes a sequence and makes probes of a given length'''
+               probe_length = 90,
+               biotin=False,
+               blat=True,
+               dfam=True,
+               **kwargs):
+    '''Takes a sequence and makes probes of a given length
+    
+    Parameters
+    __________
+    
+    fasta : str
+        Path to a fasta file containing the sequence to 
+        generate probes against
+        
+    gene : str
+        The name of the target gene, used to name probes
+        and the output file
+        
+    probe_length : int
+        The total length of the probe in nucleotides. If
+        an adaptor is used, this length includes the length
+        of the adapter
+        
+    biotin : Bool
+        Whether to add a 5'-biotin to the probes. Formatted
+        for ordering from Integrated DNA Technologies (IDT).
+        Default False
+        
+    blat : Bool
+        Whether to filter probes for multiple genome matches 
+        using UCSC BLAT. If True, the genome assembly name 
+        must be supplied to **kwargs. Default True
+        
+    dfam : Bool
+        Whether to filter probes for transposable elements and
+        tandem repeats using the Institute of Systems Biology's 
+        Dfam database. If True, the species name must be supplied
+        to **kwargs. Default True
+        
+    **kwargs : dictionary
+    
+        genome : str
+            Used for BLAT filtering. Short assembly name for the 
+            species genome as listed in BLAT, e.g. 'hg38,' 'mm39,' 
+            or 'dm6'
+
+        tolerance : int
+            Used for BLAT filtering. Number of acceptable matches 
+            to other genomic loci. Default 25
+    
+        species : str
+            DFAM species to check repeats, e.g. "Homo sapiens",
+            "Mus musculus", or "Drosophila melanogaster"
+    
+    Returns
+    _______
+    
+    output : a Pandas DataFrame
+        A dataframe containing the final probes after filtering
+        steps. Identical to the Probes.csv file
+    
+    rapProbesLog.out : a text file
+        A text file containing a log of steps taken by the 
+        rap_probes function
+        
+    [gene]_[probe_length]ntProbes.csv : a csv file
+        A csv file containing the final probes. Identical
+        to the Pandas Dataframe ouput
+        
+    blatFailedProbes.csv : a csv file
+        If performing BLAT filtering, a csv file containing BLAT
+        results for probes that did not pass filters
+    
+    blatPassedProbes.csv : a csv file
+        If performing BLAT filtering, a csv file containing BLAT
+        results for probes that passed filters
+        
+    dfamFailedProbes.csv : a csv file
+        If performing Dfam filtering, a csv file containing Dfam
+        results for probes that did not pass filters
+    
+    '''
+    
+    # Create log file
+    log = open('rapProbesLog.out','w')
+    log.writelines('Probe Design Log for ' + gene)
+    
     # Read file
     with open(fasta,'r') as f:
         seq = f.readlines()
@@ -92,17 +415,71 @@ def rap_probes(fasta,
     
     s_seq = [str(i.reverse_complement()) for i in s_seq]
     
+    # Update log
+    log.writelines('Original probes generated: ' + str(len(s_seq)))
+    
+    # Filter with BLAT
+    if blat == True:
+        print('Starting BLAT')
+        keepdf, dropdf = blatfilt(seq,
+                                 kwargs['genome'],
+                                 s_seq,
+                                 tolerance=25)
+        
+        # Export probes
+        dropdf.to_csv('blatFailedProbes.csv',index=False)
+        keepdf.to_csv('blatPassedProbes.csv',index=False)
+        
+        # Update log
+        log.writelines('Probes removed by BLAT: ' + 
+                       str(len(dropdf)))
+        log.writelines('Probes remaining after BLAT: ' + 
+                       str(len(keepdf)))
+        
+        # Update seq list
+        blatkeep = np.unique(keepdf['qName'])
+        s_seq = [s_seq[int(i)] for i in blatkeep]
+        
+        print('BLAT Done')
+        
+    else:
+        pass
+    
+    # Filter with Dfam
+    if dfam == True:
+        print('Starting Dfam')
+        keep_probes, drop_probes = dfamfilt(s_seq, kwargs['species'])
+        
+        # Update log
+        log.writelines('Probes removed by Dfam: ' + 
+                       str(len(drop_probes)))
+        log.writelines('Probes remaining after Dfam: ' + 
+                      str(len(keep_probes)))
+        
+        # Update seq list
+        s_seq = [s_seq[int(i)] for i in keep_probes]
+    
+    else:
+        pass
+    
     # Name the probes and return a dataframe
     prb_nms = [gene + '_' + str(i+1) for i in range(len(s_seq))]
-    
-    # Export fasta
-    with open(gene + '_prefilt.fasta','w') as p:
-        for i,v in enumerate(s_seq):
-            p.write('>'+prb_nms[i]+'\n')
-            p.write(v + '\n')
             
     # Add adapt seq
     s_seq = [adaptseq + i for i in s_seq]
     
-    return pd.DataFrame({'Name':prb_nms,
+    # Add biotin
+    if biotin == True:
+        ['/5Biosg/' + i for i in s_seq]
+        
+    else:
+        pass
+    
+    fname = gene + '_' + str(probe_length) + 'ntProbes.csv'
+    
+    df =  pd.DataFrame({'Name':prb_nms,
                         'Sequence':s_seq})
+    
+    df.to_csv(fname, index=False)
+    
+    return df
